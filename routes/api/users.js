e477f50aeee78007e0c2260e1de2245c9dbac20c
George Lame <lame.giorgos@gmail.com>
2020-06-03 13:04:20 +0300

    Finished Backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 // @route       POST api/users
@@ -31,7 +33,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -53,9 +57,21 @@ router.post(
 
       await user.save();
 
-      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('User Registered');
+      jwt.sign(
+        payload,
+        config.get('jwtsecret'),
+        { expiresIn: 36000000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
